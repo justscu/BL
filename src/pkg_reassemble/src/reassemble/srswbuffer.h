a49bda548851560de367224a1c504007ffa0fbc2
justscu <justscu@163.com>
2022-01-28 17:44:19 +0800

    U

--- src/pkg_reassemble/src/reassemble/srswbuffer.h
+++ src/pkg_reassemble/src/reassemble/srswbuffer.h
@@ -2,36 +2,39 @@
 
 #include <stdint.h>
 #include "headers.h"
+#include "utils.h"
 
-// single-read, single-write buffer.
 class SrSwBuffer {
 public:
     struct Cell {
         cap_hdr  hd;
-        const char *str = nullptr;
+        const char *str = nullptr; // without pcap header.
     };
 
 public:
+    SrSwBuffer() {}
+    ~SrSwBuffer() {}
+    SrSwBuffer(const SrSwBuffer&) = delete;
+    SrSwBuffer& operator=(const SrSwBuffer&) = delete;
+
     bool init();
     void unInit();
-
     void reset();
 
+    // str: 去掉pcap头的数据
     bool write(const cap_hdr *hd, const char *str);
-    bool read(cap_hdr *hd, const char * &str);
+    bool read(cap_hdr *hd, const char* &str);
 
 private:
     // cycle buffer: saving raw data.
     int32_t      wpos_ = 0;
     char         *buf_ = nullptr; // save raw data.
-    const int32_t buf_sent_size =   4*1024*1024; // sentry size
+    const int32_t buf_sent_size =   4*1024*1024; // sentry size, 4K.
     const int32_t buf_size      = 256*1024*1024;
 
 private:
-    volatile uint64_t vec_widx_ = 0;
-    volatile uint64_t vec_ridx_ = 0;
-    Cell             *vec_ = nullptr;
-    const uint64_t    vec_size = buf_size / PKG_AVG_SIZE;
+    SPSCQueue      que_;
+    const uint32_t que_size = buf_size / PKG_AVG_SIZE;
 
 private:
     uint64_t total_wt_len_ = 0; // write
