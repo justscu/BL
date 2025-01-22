eff4cf2ec9ce468963b17eb36a2fa26d2a792223
justscu <justscu@163.com>
2025-01-22 15:17:27 +0800

    U

--- src/commlib/utils/utils_stl_mempool.h
+++ src/commlib/utils/utils_stl_mempool.h
@@ -3,7 +3,7 @@
 #include <new>
 #include <stdint.h>
 #include <cstddef>
-
+#include <mutex>
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 使用链表实现内存池
@@ -25,7 +25,7 @@ public:
     ~UtilsMemPool() { uninit(); }
 
 public:
-    //
+    // 初始化内存池
     // capacity: 内存池默认大小
     bool init(uint64_t capacity) {
         for (uint64_t i = 0; i < capacity; ++i) {
@@ -37,6 +37,7 @@ public:
         return true;
     };
 
+    // 释放内存池
     void uninit() {
         Block *cur = free_blocks_;
         while (cur) {
@@ -49,7 +50,7 @@ public:
     }
 
     //
-    // 申请一块内存池，当超过默认大小时，会调用new进行分配
+    // 申请一块内存，当超过默认大小时，会调用new进行分配
     Type* new_buf() {
         if (free_blocks_) {
             Block *ret = free_blocks_;
@@ -62,6 +63,7 @@ public:
         }
     }
 
+    // 释放一块内存
     void del_buf(Type *buf) {
         Block *b = reinterpret_cast<Block*>(reinterpret_cast<char*>(buf) - offsetof(Block, T));
         b->next  = free_blocks_;
@@ -69,6 +71,7 @@ public:
         free_blocks_ = b;
     }
 
+    // 获取空闲内存块的数量
     uint64_t free_size() const {
         uint64_t ret = 0;
 
@@ -83,4 +86,88 @@ public:
 
 private:
     Block *free_blocks_ = nullptr;
+};
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 使用链表实现内存池
+//  添加互斥锁, 线程安全
+//  支持多个线程同时访问和修改共享资源
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+template<typename DataType>
+class UtilsMemPoolWithLock {
+public:
+    using Type = DataType;
+
+    struct alignas(8) Block {
+        Block *next;
+        Type      T;
+
+        Block() : next(nullptr), T(Type()) { }
+    };
+
+    ~UtilsMemPoolWithLock() { uninit(); }
+
+public:
+    // 初始化内存池
+    bool init(uint64_t capacity) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        for (uint64_t i = 0; i < capacity; ++i) {
+            Block *b = new Block;
+            b->next = free_blocks_;
+            free_blocks_ = b;
+        }
+        return true;
+    }
+
+    // 销毁内存池
+    void uninit() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        Block *cur = free_blocks_;
+        while (cur) {
+            Block *next = cur->next;
+            delete cur;
+            cur = next;
+        }
+        free_blocks_ = nullptr;
+    }
+
+    // 申请一块内存
+    Type* new_buf() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (free_blocks_) {
+            Block *ret = free_blocks_;
+            free_blocks_ = free_blocks_->next;
+            return &(ret->T);
+        } else {
+            Block *ret = new Block;
+            return &(ret->T);
+        }
+    }
+
+    // 释放一块内存
+    void del_buf(Type *buf) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        ptrdiff_t offset = offsetof(Block, T);
+        Block *b = reinterpret_cast<Block*>(reinterpret_cast<char*>(buf) - offset);
+        b->next = free_blocks_;
+        free_blocks_ = b;
+    }
+
+    // 获取空闲内存块的数量
+    uint64_t free_size() const {
+        std::lock_guard<std::mutex> lock(mutex_);
+        uint64_t ret = 0;
+        Block *cur = free_blocks_;
+        while (cur) {
+            ++ret;
+            cur = cur->next;
+        }
+        return ret;
+    }
+
+private:
+    Block *free_blocks_ = nullptr;
+    mutable std::mutex mutex_;
 };
